7e2e1ff77ebd3d61cac7ce0ceeb755e041cd6ef7
mvdevelop <marcosvmdilly@gmail.com>
2026-06-01 16:33:14 -0300

    fix: fixing swagger

--- backend/MacawEnglishSchool.API/Program.cs
+++ backend/MacawEnglishSchool.API/Program.cs
@@ -51,11 +51,8 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Use custom middlewares
 app.UseMiddleware<LoggingMiddleware>();
